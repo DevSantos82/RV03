9235894ef4f088a2da387a53129d98758bc97724
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-19 17:04:15 -0300

    Update __init__.py

--- comunidadeclp/__init__.py
+++ comunidadeclp/__init__.py
@@ -3,13 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
 
-database = SQLAlchemy(app)
 
 
 from comunidadeimpressionadora import routes
