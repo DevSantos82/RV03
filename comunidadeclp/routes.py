5c666f3f76b4b66cfc44f4d123b7939e227c25bd
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-19 16:32:59 -0300

    Update routes.py

--- comunidadeclp/routes.py
+++ comunidadeclp/routes.py
@@ -1,1 +1,14 @@
+from flask import render_template
+from comunidadeclp import app
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
 
