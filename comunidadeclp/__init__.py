7c5eb57acdec779ce9e561b1187d9034d03fc214
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-19 18:00:37 -0300

    Update __init__.py

--- comunidadeclp/__init__.py
+++ comunidadeclp/__init__.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
 
 database = SQLAlchemy(app)
 
