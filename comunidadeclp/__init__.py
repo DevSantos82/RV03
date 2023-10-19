4b2f1a6fbf15dcbd21631c47d08620ffcd461a92
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-19 17:49:04 -0300

    Update __init__.py

--- comunidadeclp/__init__.py
+++ comunidadeclp/__init__.py
@@ -1,12 +1,20 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
-
-database = SQLAlchemy(app)
+from comunidadeclp import database
+from datetime import datetime
 
 
-from comunidadeclp import routes
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    senha = database.Column(database.String, nullable=False)
+    foto_perfil = database.Column(database.String, default='default.jpg')
+    posts = database.relationship('Post', backref='autor', lazy=True)
+    cursos = database.Column(database.String, nullable=False, default='Não Informado')
+
+
+class Post(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    titulo = database.Column(database.String, nullable=False)
+    corpo = database.Column(database.Text, nullable=False)
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
