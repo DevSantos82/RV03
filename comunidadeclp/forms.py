29f4ec6717cb9a715b87d8bc62046cf91d485b67
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-19 16:41:24 -0300

    Update forms.py

--- comunidadeclp/forms.py
+++ comunidadeclp/forms.py
@@ -1,46 +1,19 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
-from forms import FormLogin, FormCriarConta
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
-
-app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
-
-database = SQLAlchemy(app)
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
 
 
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
-
-
-@app.route('/usuarios')
-def usuarios():
-    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form_login = FormLogin()
-    form_criarconta = FormCriarConta()
-    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-        return redirect(url_for('home'))
-    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
-        return redirect(url_for('home'))
-    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
 
