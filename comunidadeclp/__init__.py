a1db48cebdd66286284e594b04a24b67471a2dca
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-20 09:46:22 -0300

    Update __init__.py

--- comunidadeclp/__init__.py
+++ comunidadeclp/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 import os
 app = Flask(__name__)
 
@@ -14,8 +13,6 @@ else:
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'alert-info'
+
 
 from comunidadeclp import routes
