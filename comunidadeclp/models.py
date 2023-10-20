fab6dd886a023c209e739fbde06309a8969c35ae
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-20 13:08:10 -0300

    Update models.py

--- comunidadeclp/models.py
+++ comunidadeclp/models.py
@@ -1,6 +1,11 @@
-from comunidadeclp import database
+from comunidadeclp import database, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
 
 class Usuario(database.Model,UserMixin):
     id = database.Column(database.Integer, primary_key=True)
@@ -10,6 +15,9 @@ class Usuario(database.Model,UserMixin):
     foto_perfil = database.Column(database.String, default='default.jpg')
     posts = database.relationship('Post', backref='autor', lazy=True)
     cursos = database.Column(database.String, nullable=False, default='Não Informado')
+
+    def contar_posts(self):
+        return len(self.posts)
 
 
 class Post(database.Model):
