f85968bebfc155e97248fb89b6eedb687b277a45
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-20 09:55:29 -0300

    Update routes.py

--- comunidadeclp/routes.py
+++ comunidadeclp/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from comunidadeclp import app, database, bcrypt
 from comunidadeclp.forms import FormLogin, FormCriarConta
 from comunidadeclp.models import Usuario
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user, login_required
 lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
 
