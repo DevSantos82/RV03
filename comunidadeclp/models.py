2cf3f59c02ffe175391d9970ea64a76c7ba5c459
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-20 10:59:32 -0300

    Update models.py

--- comunidadeclp/models.py
+++ comunidadeclp/models.py
@@ -1,8 +1,8 @@
 from comunidadeclp import database
 from datetime import datetime
+from flask_login import UserMixin
 
-
-class Usuario(database.Model):
+class Usuario(database.Model,UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
