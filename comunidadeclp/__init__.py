04a21529004f8891313c2c382db51f1207d029fc
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-20 09:56:55 -0300

    Update __init__.py

--- comunidadeclp/__init__.py
+++ comunidadeclp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from Flask_login import LoginManager
 import os
 
 
