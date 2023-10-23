883be4f8b549935e1cae5fcd5d10460671243b83
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-23 07:47:08 -0300

    Update routes.py

--- comunidadeclp/routes.py
+++ comunidadeclp/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request, abort
-from comunidadeclp import app, database, bcrypt, app_1
+from comunidadeclp import app, database, bcrypt
 from comunidadeclp.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from comunidadeclp.models import Usuario, Post
 from flask_login import login_user, logout_user, current_user, login_required
@@ -29,9 +29,14 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
-   if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-        return redirect(url_for('home'))
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Falha no Login. E-mail ou Senha Incorretos', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
