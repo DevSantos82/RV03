4d32c37b60c262b825634492484baffe3c310434
DevSantos82 <147875227+DevSantos82@users.noreply.github.com>
2023-10-23 07:38:55 -0300

    Update routes.py

--- comunidadeclp/routes.py
+++ comunidadeclp/routes.py
@@ -30,8 +30,8 @@ def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-       flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-       return redirect(url_for('home'))
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
